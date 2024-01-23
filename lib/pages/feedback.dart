127c678ccbedd10f9cc94c6241621abf5b74acbf
siavash rahimi <rahimi.f.h@gmail.com>
2024-01-23 13:29:05 +0330

    feat(pages) add four new page to project

--- lib/pages/feedback.dart
+++ lib/pages/feedback.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:start/pages/simple_calculate_page.dart';
 
-class History extends StatelessWidget {
-  const History({super.key});
+class FeedBack extends StatelessWidget {
+  const FeedBack({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Second Route'),
+        title: const Text('feedback'),
       ),
       body: Center(
         child: Column(
           children: [
-            const Text("history"),
+            const Text("feedback"),
             ElevatedButton(
               onPressed: () {
                 Navigator.push(
