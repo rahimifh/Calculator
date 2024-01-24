8cc7f9d51ef4760c0c74c187822b6c31ee442e02
Amirreza_Chegini <amirreza.chegini.1380@gmail.com>
2024-01-24 12:15:02 +0330

    fix: change title of history page

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:start/models/history.dart';
-import 'package:start/pages/simple_calculate_page.dart';
 
 class HistoryPage extends StatefulWidget {
   const HistoryPage({super.key});
@@ -16,15 +15,16 @@ class _HistoryPageState extends State<HistoryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Second Route'),
-        ),
-        body: SafeArea(
-            child: ListView.builder(
+      appBar: AppBar(title: const Text('History')),
+      body: SafeArea(
+        child: ListView.builder(
           itemCount: box.values.length,
           itemBuilder: (context, index) => ListTile(
-              title: Text(box.values.toList()[index].operatios),
-              subtitle: Text('${box.values.toList()[index].date}')),
-        )));
+            title: Text(box.values.toList()[index].operatios),
+            subtitle: Text('${box.values.toList()[index].date}'),
+          ),
+        ),
+      ),
+    );
   }
 }
